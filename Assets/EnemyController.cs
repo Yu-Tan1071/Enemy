9c2de133e9be3ac3927d15576ed05e8343020439
yu-tan <yu.tan10714@gmail.com>
2022-02-24 01:09:29 +0900

    BulletがEnemyに当たると消える機能追加

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Bullet")
+        {
+            Destroy(gameObject, 0.1f);
+        }      
+    }
+}
---
+++ Assets/Shooter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public GameObject prefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject obj = Instantiate(prefab);
+            obj.transform.parent = transform;
+            obj.transform.localPosition = Vector3.zero;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 dir = ray.direction.normalized;
+            obj.GetComponent<Rigidbody>().velocity = dir * 100.0f;
+        }
+    }
+}
