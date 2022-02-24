eeece62915d4a5701e1d1510e53e688323298ecf
yu-tan <yu.tan10714@gmail.com>
2022-02-25 02:10:21 +0900

    回転追加

---
+++ Assets/CameraController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject mainCamera;      
+    public GameObject subCamera;        
+
+
+    
+    void Start()
+    {
+        
+        mainCamera = GameObject.Find("MainCamera");
+        subCamera = GameObject.Find("SubCamera");
+
+        
+        subCamera.SetActive(false);
+    }
+
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // 各カメラオブジェクトの有効フラグを逆転(true→false,false→true)させる
+            mainCamera.SetActive(!mainCamera.activeSelf);
+            subCamera.SetActive(!subCamera.activeSelf);
+        }
+
+        /*
+        if (Input.GetKey(KeyCode.Space))
+        {
+            subCamera.SetActive(true);
+            mainCamera.SetActive(false);
+        }
+        else
+        {
+            mainCamera.SetActive(true);
+            subCamera.SetActive(false);
+        }
+        */
+    }
+}
---
+++ Assets/CharacterController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    Vector3 pos;
+    Quaternion rot;
+
+    void Start()
+    {
+        pos = transform.position;
+        rot = transform.rotation;
+    }
+    void Update()
+    {
+        float leftRight = Input.GetAxis("Horizontal");
+        float frontBack = Input.GetAxis("Vertical");
+        pos.x += leftRight;
+        pos.z += frontBack;
+        transform.position = pos;
+        
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            //Quaternion rot = new Quaternion(0f, 90f, 0f, 0f);
+            rot.y += 45;
+            transform.rotation = Quaternion.Euler(0f, rot.y, 0f);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            //Quaternion rot = new Quaternion(0f, -90f, 0f, 0f);
+            rot.y -= 45;
+            transform.rotation = Quaternion.Euler(0f, rot.y, 0f);
+        }
+        
+    }
+}
