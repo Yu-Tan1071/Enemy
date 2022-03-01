733306c4954311b849a75de7b15c4b04439bed2b
yu-tan <yu.tan10714@gmail.com>
2022-03-01 22:52:22 +0900

    CharController追加

---
+++ Assets/Char3Controller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Char3Controller : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey("up"))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            transform.position += transform.forward * 0.1f;
+        }
+
+        if (Input.GetKey("down"))
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            transform.position += transform.forward * 0.1f;
+        }
+
+
+        if (Input.GetKey("right"))
+        {
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+            transform.position += transform.forward * 0.1f;
+        }
+
+        if (Input.GetKey("left"))
+        {
+            transform.rotation = Quaternion.Euler(0, 270, 0);
+            transform.position += transform.forward * 0.1f;
+        }
+    }
+}
